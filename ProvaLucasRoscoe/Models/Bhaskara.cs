45caecadaff3809f7618364b6b2dd2b56c8255a7
lucascardosoroscoe <lucascardosoroscoe@gmail.com>
2021-06-18 20:09:12 -0400

    Adicionar arquivos de projeto.

---
+++ ProvaLucasRoscoe/Models/Bhaskara.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bhaskara
+{
+    class Bhaskara
+    {
+        public void Main(string[] args)
+        {
+            double a, b, c;
+
+            double resultado = calcularBhaskara(a, b, c);
+
+        }
+
+        public void calcularBhaskara(double a, double b, double c)
+        {
+            double delta = Math.Pow(b, 2) - (4 * a * c));
+            double x1 = (((b * (-1)) + Math.Sqrt(delta)) / (2 * a));
+            double x2 = (((b * (-1)) - Math.Sqrt(delta)) / (2 * a));
+            if (delta > 0)
+            {
+                if(x1 >= x2)
+                {
+                    return x1;
+                }
+                else
+                {
+                    return x2;
+                }
+            }
+            else if (delta == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }            
+        }
+    }
+}
---
+++ ProvaLucasRoscoe/Models/Pessoa.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pessoa
+{
+    class Pessoa
+    {
+        public void Main(string[] args)
+        {
+            string nome;
+            int idade;
+
+            string primo = calculaNumeroPrimo(idade);
+        }
+
+        public void calculaNumeroPrimo(int idade)
+        {
+            string resultado;
+            try
+            {
+                for (int i = 2; i < idade; i++)
+                {
+                    int resto = idade % i;
+                    if (resto == 0)
+                    {
+                        resultado = idade + " não é um número primo";
+                        i = idade + 1;
+                    }
+                    else
+                    {
+                        resultado = idade + " é um número primo";
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro ao Verificar nº primo");
+                Console.WriteLine(e);
+            }
+
+            return resultado;
+        }
+
+    }
+}
